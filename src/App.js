c68a435a6929faa346fba96bb3775c3311c89d1e
erl jocson <erl.jocson@teamhgs.com>
2024-05-27 23:53:07 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Category from "./components/Category";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer className="App">
+      <ContentContainer>
+        <WhiteImageContainer src="/logo/hgs white logo.svg" alt="" />
+        <CategoryContainer>
+          <Category />
+        </CategoryContainer>
+      </ContentContainer>
+    </MainContainer>
   );
 }
 
 export default App;
+
+const MainContainer = styled.div`
+  position: absolute;
+  background-image: url("/GIF/BG.gif");
+  background-position: center;
+  background-size: cover;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+`;
+
+const ContentContainer = styled.div`
+  height: 100%;
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const WhiteImageContainer = styled.img`
+  height: 300px;
+  width: 600px;
+`;
+
+const CategoryContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
